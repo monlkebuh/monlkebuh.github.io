3e0ef5093ca70c77c554c20602a60d7eb02d0976
aaibby <104386339+aaibby@users.noreply.github.com>
2023-04-21 10:42:28 -0600

    Update favicon.js

--- favicon.js
+++ favicon.js
@@ -1,5 +1,1 @@
-javascript:function
-gcloak() {
-  var link =
-      document.querySelector("link[rel*='icon']")
-  || document.createElement('link');link.type = 'image/x-icon';link.rel = 'shortcut icon';link.href = 'https://raw.githubusercontent.com/aaibby/aaibby.github.io/main/fav.png';document.title = 'Aaibby';console.log(document.title);document.getElementsByTagName('head')[0].appendChild(link) };gcloak();setInterval(gcloak, 1000);
+javascript:function gcloak() { var link = document.querySelector("link[rel*='icon']") || document.createElement('link');link.type = 'image/x-icon';link.rel = 'shortcut icon';link.href = 'https://raw.githubusercontent.com/aaibby/aaibby.github.io/main/fav.png';document.title = 'Aaibby';console.log(document.title);document.getElementsByTagName('head')[0].appendChild(link) };gcloak();setInterval(gcloak, 1000);
